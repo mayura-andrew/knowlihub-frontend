456f1646fc2a3674485ce456d29bc96c8d7563df
mayura-andrew <mayuraalahakoon@gmail.com>
2024-11-21 12:18:35 +0530

    Remove commented-out RecommendList component and its props interface

--- src/components/features/RecommentList.component.tsx
+++ src/components/features/RecommentList.component.tsx
@@ -1,23 +1,21 @@
-import { RecommendProps } from "@/types/types";
-import Recommend from "../common/Recommend.component";
 
-interface RecommendListProps {
-    recommends: RecommendProps[];
-}
+// interface RecommendListProps {
+//     recommends: RecommendProps[];
+// }
 
-const RecommendList: React.FC<RecommendListProps> = ({ recommends }) => {
-    return (
-      <div className="mt-4">
-        {recommends.map((recommend: RecommendProps, index) => (
-          <Recommend
-            key={index}
-            author={recommend.author}
-            text={recommend.text}
-            avatar={recommend.avatar}
-          />
-        ))}
-      </div>
-    );
-  };
+// const RecommendList: React.FC<RecommendListProps> = ({ recommends }) => {
+//     return (
+//       <div className="mt-4">
+//         {recommends.map((recommend: RecommendProps, index) => (
+//           <Recommend
+//             key={index}
+//             author={recommend.author}
+//             text={recommend.text}
+//             avatar={recommend.avatar}
+//           />
+//         ))}
+//       </div>
+//     );
+//   };
   
-  export default RecommendList;
+//   export default RecommendList;
